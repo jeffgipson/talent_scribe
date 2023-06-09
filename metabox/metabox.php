170fb4bde70cf9a53505ed31b0e922ec1867a937
Jeff Gipson <jeff@recruiterswebsites.com>
2023-06-09 09:41:40 -0500

    Fix image dupe issue

--- metabox/metabox.php
+++ metabox/metabox.php
@@ -6,7 +6,7 @@ function hcf_register_meta_boxes() {
     add_meta_box(
         'hcf-1',
         __('<div><i class="fa-sharp fa-solid fa-magnifying-glass"></i> Talent Scribe AI SEO & Images</div>',
-        'hcf'),
+            'hcf'),
         'hcf_display_callback',
         'post',
         'normal',
@@ -43,69 +43,89 @@ function hcf_save_meta_box($post_id)
         'hcf_image',
     ];
     foreach ($fields as $field) {
+
+        $update_image = true;
+
+
+        if (isset($_POST['hcf_image'])){
+
+            if ($_POST['hcf_image'] == get_post_meta($post_id, 'hcf_image', true)) {
+                $update_image = false;
+            }
+        }
+        else{
+            $update_image = false;
+
+        }
+
         if (array_key_exists($field, $_POST)) {
             update_post_meta($post_id, $field, sanitize_text_field($_POST[$field]));
 
         }
     }
 //        if hcf_image is set, then we need to set the featured image
-       if (isset($_POST['hcf_image']))  {
+    if ( $update_image)  {
 
-           //if $_POST['hcf_image'] is blank then set the featured image to the default image
-              if ($_POST['hcf_image'] == '') {
-                  $image_url = esc_url(plugins_url('/assets/default-image.png', __FILE__));
-              } else {
-                  $image_url = $_POST['hcf_image']; // Define the image URL here
-              }
+        //if $_POST['hcf_image'] is blank then set the featured image to the default image
+        if ($_POST['hcf_image'] == '') {
+            $image_url = esc_url(plugins_url('/assets/default-image.png', __FILE__));
+        } else {
+            $image_url = $_POST['hcf_image']; // Define the image URL here
+        }
+
+
 
 //            $image_url = $_POST['hcf_image']; // Define the image URL here
-           //get post title
-              $post_title = get_the_title($post_id);
-              //convert post title to slug
-                $post_slug = sanitize_title($post_title);
-            $image_name = $post_slug.'.png';
-            $upload_dir = wp_upload_dir(); // Set upload folder
-            $image_data = file_get_contents($image_url); // Get image data
-            $unique_file_name = wp_unique_filename($upload_dir['path'], $image_name); // Generate unique name
-            $filename = basename($unique_file_name); // Create image file name
+        //get post title
+        $post_title = get_the_title($post_id);
+        //convert post title to slug
+        $post_slug = sanitize_title($post_title);
+        $image_name = $post_slug.'.png';
+        $upload_dir = wp_upload_dir(); // Set upload folder
+        $image_data = file_get_contents($image_url); // Get image data
+        $unique_file_name = wp_unique_filename($upload_dir['path'], $image_name); // Generate unique name
+        $filename = basename($unique_file_name); // Create image file name
 
 // Check folder permission and define file location
-            if (wp_mkdir_p($upload_dir['path'])) {
-                $file = $upload_dir['path'] . '/' . $filename;
-            } else {
-                $file = $upload_dir['basedir'] . '/' . $filename;
-            }
+        if (wp_mkdir_p($upload_dir['path'])) {
+            $file = $upload_dir['path'] . '/' . $filename;
+        } else {
+            $file = $upload_dir['basedir'] . '/' . $filename;
+        }
 
 // Create the image  file on the server
-            file_put_contents($file, $image_data);
-
+        file_put_contents($file, $image_data);
+//
 // Check image file type
-            $wp_filetype = wp_check_filetype($filename, null);
-
+        $wp_filetype = wp_check_filetype($filename, null);
+//
 // Set attachment data
-            $attachment = array(
-                'post_mime_type' => $wp_filetype['type'],
-                'post_title' => sanitize_file_name($filename),
-                'post_content' => '',
-                'post_status' => 'inherit'
-            );
-
+        $attachment = array(
+            'post_mime_type' => $wp_filetype['type'],
+            'post_title' => sanitize_file_name($filename),
+            'post_content' => '',
+            'post_status' => 'inherit'
+        );
+//
 // Create the attachment
-            $attach_id = wp_insert_attachment($attachment, $file, $post_id);
+        $attach_id = wp_insert_attachment($attachment, $file, $post_id);
 
 // Include image.php
-            require_once(ABSPATH . 'wp-admin/includes/image.php');
+        require_once(ABSPATH . 'wp-admin/includes/image.php');
 
 // Define attachment metadata
-            $attach_data = wp_generate_attachment_metadata($attach_id, $file);
+        $attach_data = wp_generate_attachment_metadata($attach_id, $file);
 
 // Assign metadata to attachment
-            wp_update_attachment_metadata($attach_id, $attach_data);
+        wp_update_attachment_metadata($attach_id, $attach_data);
 
 // And finally assign featured image to post
-            set_post_thumbnail($post_id, $attach_id);
+        set_post_thumbnail($post_id, $attach_id);
 
-        }
+        update_post_meta($attach_id, '_wp_attachment_image_alt', 'Image alt text');
+
+
+    }
 
 }
 
